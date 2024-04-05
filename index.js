6ff1ca3242cedba16c2606cfc43c0a4edcdfa79a
eliud wainaina <wainainaeliud7@gmail.com>
2024-04-05 14:50:23 -0700

    trial

---
+++ index.js
@@ -0,0 +1,9 @@
+function introduction(eliud){
+    console.log(`Hi,my name is {eliud}.`);
+}
+function introductionWithLanguage(eliud, javaScript){
+    console.log(`Hi,my name is{eliud} and i am learning to program in {javascript}`);
+}
+function introductionWithLanguageo(eliud, javaScript) {
+    console.log(`Hi,my name is{eliud} and i am learning to program in {javascript}`);
+}
